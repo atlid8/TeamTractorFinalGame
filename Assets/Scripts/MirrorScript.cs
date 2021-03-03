790dd882f818da013b512752c8fd8ff90ccb47a3
totason <jonkrona@gmail.com>
2021-03-03 17:11:21 +0000

    Bætti við bullet og mirror scriptu og mirror reflectar bullets, bullets deyja eftir ákveðinn tíma

---
+++ Assets/Scripts/MirrorScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+    bool reflects = true;
+    void Start()
+    {
+        
+    }
+}
