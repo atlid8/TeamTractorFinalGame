470551233c8ab86d60bb4616fbd1a6e64eb6b86b
Daníel Aron Davíðsson <danield18@ru.is>
2021-03-10 14:44:54 +0000

    fixed mass on player

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -7,6 +7,7 @@ public class MouseCursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
