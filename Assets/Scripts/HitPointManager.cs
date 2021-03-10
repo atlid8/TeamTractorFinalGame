cf63e7cdd5a63d2a444e0885de990e17e67c2ba4
totason <jonkrona@gmail.com>
2021-03-10 14:18:01 +0000

    cameraShake þegar player er skotinn, þarf bara að setja CM vcam1 í camera og haka í shakecamera í hitpoint manager

--- Assets/Scripts/HitPointManager.cs
+++ Assets/Scripts/HitPointManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class HitPointManager : MonoBehaviour
 {
+    public bool shakeScreen = false;
     public int currentHitPoints;
+    public GameObject camera;
+    private CameraShake cameraShake;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraShake = camera.GetComponent<CameraShake>();
     }
 
     // Update is called once per frame
@@ -19,6 +22,9 @@ public class HitPointManager : MonoBehaviour
     public void takeDamage(int damageAmount)
     {
         currentHitPoints -= damageAmount;
+        if (shakeScreen){
+            cameraShake.ShakeCamera();
+        }
         if (currentHitPoints <= 0)
         {
             if (transform.parent != null){
