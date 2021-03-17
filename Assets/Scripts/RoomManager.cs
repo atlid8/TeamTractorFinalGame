3baf13030f52d2081f872d077178921c5286f47f
Daníel Aron Davíðsson <danield18@ru.is>
2021-03-17 14:27:45 +0000

    open door script

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -48,9 +48,9 @@ public class RoomManager : MonoBehaviour
             cleared = true;
         }
         if (cleared && closedDoors){
-            if (leftDoor){anim = leftDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); leftDoor.transform.Find("Green").gameObject.SetActive(true); leftDoor.transform.Find("Red").gameObject.SetActive(false);}
-            if (rightDoor){anim = rightDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); rightDoor.transform.Find("Green").gameObject.SetActive(true); rightDoor.transform.Find("Red").gameObject.SetActive(false);}
-            if (topDoor){anim = topDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); topDoor.transform.Find("Green").gameObject.SetActive(true); topDoor.transform.Find("Red").gameObject.SetActive(false);}
+            if (leftDoor){anim = leftDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); leftDoor.transform.Find("Red").gameObject.SetActive(false);}
+            if (rightDoor){anim = rightDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); rightDoor.transform.Find("Red").gameObject.SetActive(false);}
+            if (topDoor){anim = topDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); topDoor.transform.Find("Red").gameObject.SetActive(false);}
             if (bottomDoor){anim = bottomDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true);}
             closedDoors = false;
         }
