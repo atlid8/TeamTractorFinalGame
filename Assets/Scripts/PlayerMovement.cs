6f06d14a50a350a5c83e33d93b502fafed987d13
Kormákur <korri123@gmail.com>
2021-03-17 16:21:46 +0000

    Player stoof

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,52 +10,36 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Camera cam;
     public GameObject playerArt;
-    private GameObject weapon;
+    private CharacterAnimation playerArtScript;
+    public Transform bulletRotation;
     
     Vector2 movement;
     Vector2 mousePos;
 
     private void Awake()
     {
-        weapon = playerArt.transform.Find("Weapon").gameObject;
+        playerArtScript = playerArt.GetComponent<CharacterAnimation>();
     }
 
-    private bool facingRight = false;
-
-    void Update() {
+    void Update() 
+    {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        if (playerArt.transform.position.x > mousePos.x) {
-            //Debug.Log("Right");
-            playerArt.transform.rotation = Quaternion.identity;
-            facingRight = false;
-        }
-        else {
-            //Debug.Log("Left");
-            playerArt.transform.rotation = Quaternion.Euler(0,180,0);
-            facingRight = true;
-        }
+        playerArtScript.Flip(!(playerArt.transform.position.x > mousePos.x));
     }
 
-    void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        
-        //rb.rotation = angle;
-        //weapon.rotation = angle;
-        var o = weapon.transform.eulerAngles;
-        float offset;
-        if (facingRight)
-            offset = 85;
-        else
-            offset = -85;
-        var fAngle = angle + offset;
-        if (facingRight)
-            fAngle = -fAngle;
-        weapon.transform.eulerAngles = new Vector3(o.x,o.y,fAngle);
+    void FixedUpdate() 
+    {
+        if (Math.Abs(movement.x) > 0.001 || Math.Abs(movement.y) > 0.001)
+            playerArtScript.SetRunningAnimation(true);
+        else 
+            playerArtScript.SetRunningAnimation(false);
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        var lookDir = mousePos - rb.position;
+        var angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        playerArtScript.angle = angle;
+        bulletRotation.eulerAngles = new Vector3(0, 0, angle);
     }
 }
