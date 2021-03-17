c23f0aac12ee02d555032e172fa75570aa20e06a
Daníel Aron Davíðsson <danield18@ru.is>
2021-03-17 14:19:02 +0000

    fixed opendoor script

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -15,7 +15,6 @@ public class RoomManager : MonoBehaviour
     private GameObject bottomDoor;
     private GameObject leftDoor;
     private GameObject rightDoor;
-    private List<GameObject> doorList;
     private Animator anim;
 
 
@@ -44,14 +43,14 @@ public class RoomManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (numberOfEnemies <= 0){
+        if (numberOfEnemies <= 0 && !cleared){
             timeManager.stop = true;
             cleared = true;
         }
         if (cleared && closedDoors){
-            if (leftDoor){anim = leftDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true);}
-            if (rightDoor){anim = rightDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true);}
-            if (topDoor){anim = topDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true);}
+            if (leftDoor){anim = leftDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); leftDoor.transform.Find("Green").gameObject.SetActive(true); leftDoor.transform.Find("Red").gameObject.SetActive(false);}
+            if (rightDoor){anim = rightDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); rightDoor.transform.Find("Green").gameObject.SetActive(true); rightDoor.transform.Find("Red").gameObject.SetActive(false);}
+            if (topDoor){anim = topDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true); topDoor.transform.Find("Green").gameObject.SetActive(true); topDoor.transform.Find("Red").gameObject.SetActive(false);}
             if (bottomDoor){anim = bottomDoor.GetComponent<Animator>(); anim.SetBool("openDoors", true);}
             closedDoors = false;
         }
