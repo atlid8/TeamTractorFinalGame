8fbcaf5651ae0a1a07d0c7ab867ed988ca5b6766
Daníel Aron Davíðsson <danield18@ru.is>
2021-04-08 17:36:30 +0000

    mouse appears now

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -9,6 +9,7 @@ public class DeathScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = true;
         level = PlayerPrefs.GetInt("level");
         GameManager.instance.globalTimeMult = 1;
         SlowMotionPickupScript.exists = false;
